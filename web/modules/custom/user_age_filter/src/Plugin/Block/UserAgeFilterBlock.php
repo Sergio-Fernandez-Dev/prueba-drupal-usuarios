e238b24f9f0f181d4b55ff783f3ee28a7a6a80af
Sergio-Fernandez-Dev <sr.sergiofernandezfernandez@gmail.com>
2023-03-31 12:31:52 +0200

    adding country and age fields to user entity

--- web/modules/custom/user_age_filter/src/Plugin/Block/UserAgeFilterBlock.php
+++ web/modules/custom/user_age_filter/src/Plugin/Block/UserAgeFilterBlock.php
@@ -11,32 +11,36 @@ use Drupal\Core\Block\BlockBase;
  * )
  */
 
-class UserAgeFilterBlock extends BlockBase {
+class UserAgeFilterBlock extends BlockBase
+{
 
-    public function build() {
-        
-        return [
-            '#type' => 'markup',
-            '#markup' => 
-              '18-35 years: ' . $this->get_number_of_user_by_age_range(18, 35) . ' || ' .
-              '35-50 years: ' . $this->get_number_of_user_by_age_range(35, 50) . ' || ' .
-              '+50 years: ' . $this->get_number_of_user_by_age_range(50),
-        ];
+  public function build()
+  {
+
+    return [
+      '#type' => 'markup',
+      '#markup' =>
+      '18-35 years: ' . $this->get_number_of_user_by_age_range(18, 35) . ' || ' .
+      '35-50 years: ' . $this->get_number_of_user_by_age_range(35, 50) . ' || ' .
+      '+50 years: ' . $this->get_number_of_user_by_age_range(50),
+    ];
+  }
+
+  public function get_number_of_user_by_age_range($min_age, $max_age = NULL)
+  {
+
+    $user_list = \Drupal::entityTypeManager()->getStorage('user')->loadMultiple();
+    $number_of_users_in_range = 0;
+
+    foreach ($user_list as $user) {
+      $age = $user->get('field_age')->value;
+
+      if ($age >= $min_age && ($age < $max_age || !$max_age)) {
+        $number_of_users_in_range++;
+      }
     }
 
-    public function get_number_of_user_by_age_range($min_age, $max_age = NULL) {
-        
-        $user_list = \Drupal::entityTypeManager()->getStorage('user')->loadMultiple();
-
-        $number_of_users_in_range = 0;        
-
-        foreach ($user_list as $user) {
-          $age = $user->get('field_age')->value;
-          if ($age >= $min_age && ($age < $max_age || !$max_age)) {
-            $number_of_users_in_range++;
-          }
-        }
-        
-        return $number_of_users_in_range;
-    }
+    return $number_of_users_in_range;
+  }
 }
+
